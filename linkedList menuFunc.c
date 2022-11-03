1bad6d640fd5ce650d72fd58909521a12e693e23
Mustafa Çaylı <90175507+mustafacayli@users.noreply.github.com>
2022-11-04 01:10:05 +0300

    Delete linkedList menuFunc.c

--- linkedList menuFunc.c
+++
@@ -1,9 +0,0 @@
-void menu(){
-    printf("\nodev1\n");
-    printf("\n\t1)---ekle\n");
-    printf("\n\t2)---sil\n");
-    printf("\n\t3)---yazma\n");
-    printf("\n\t4)---arama\n");
-    printf("\n\t0)---cikma\n");
-    printf("seciminizi yapin");
-}
